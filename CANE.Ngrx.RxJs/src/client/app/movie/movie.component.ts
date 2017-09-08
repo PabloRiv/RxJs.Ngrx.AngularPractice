4ac8340ea20ba9a5c08c5ada87ae4fc2d004b5cb
Perry Biava <perry.biava@biavaInteractive.com>
2017-09-08 02:34:51 -0400

    using viewchild for dom manipulation

--- CANE.Ngrx.RxJs/src/client/app/movie/movie.component.ts
+++ CANE.Ngrx.RxJs/src/client/app/movie/movie.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2 } from "@angular/core";
 import { Observable } from "rxjs";
+
 // import { Observable } from "rxjs/Observable";
 // import "rxjs/add/operator/map";
 // import "rxjs/add/operator/filter";
@@ -7,6 +8,7 @@ import { Observable } from "rxjs";
 
 import { Movie } from "./movie";
 import { MyObserver } from "../shared/miObserver";
+import { MovieService } from "./movie.service";
 
 
 @Component({
@@ -19,18 +21,32 @@ export class MovieComponent implements OnInit {
   //source = Observable.from(this.numbers);
   title = "Welcome to the movies"
   count = "empty";
-  @ViewChild("circleelement", {read: ElementRef}) circle: ElementRef;
+  //@ViewChild("circleelement", {read: ElementRef}) circle: ElementRef;
   //circle = document.getElementById("circle");
   //console.log(this.circle);
 
-  // demo of using with events
-  source = Observable.fromEvent(document, "mousemove")
-  .map((e: MouseEvent) => {
-    return {
-      x: e.clientX,
-      y: e.clientY
-    }
-  });
+  movies: Movie[];
+  
+  
+  // XmlHttpRequest
+  @ViewChild("output", {read: ElementRef}) output: ElementRef;
+  @ViewChild("buttonel", {read: ElementRef}) buttonn: ElementRef;
+
+  // click = Observable.fromEvent(this.button, "click");
+  click = null;
+
+  // demo of using with events. 
+  // source = Observable.fromEvent(document, "mousemove")
+  // .map((e: MouseEvent) => {
+  //   return {
+  //     x: e.clientX,
+  //     y: e.clientY
+  //   }
+  // })
+  // .filter(value => value.x < 500)
+  // .delay(300);
+
+
   // source = Observable.fromEvent(document, "mousemove")
   //   .map((e: MouseEvent) => {
   //     return {
@@ -58,22 +74,32 @@ export class MovieComponent implements OnInit {
   //   .filter(n => n > 4);
 
   // constructor(public count:Observable<number>) { 
-  constructor() { 
+  constructor(private renderer: Renderer2, private el: ElementRef, private service: MovieService) { 
      //this.count = 6;
+
   }
 
   ngOnInit() {
+    // this.service.getLocalMovies()
+    //   .subscribe(movie => this.movies.push(movie));
+
+    this.service.getLocalMovies()
+    .subscribe(movie => this.movies = movie);
+
      //this.source.map(res => res.toString()).subscribe(item => this.count = item);
      //this.source.subscribe(new MyObserver);
    
     // used with demo about events
     //console.log(circleelement);
-    this.source.subscribe(
-      //value => console.log(value),
-      value => this.onNext(value),
-      e => console.log(`error: ${e}`),
-      () => console.log("complete")
-    );
+
+    // because the source variable is subscribing to the stream,
+    // it is not just getting one set of coordinates, but,
+    // a continual stream of coordinate pairs
+    // this.source.subscribe(
+    //   value => this.onNext(value),
+    //   e => console.log(`error: ${e}`),
+    //   () => console.log("complete")
+    // );
 
 
     // used with demo about operators
@@ -82,17 +108,73 @@ export class MovieComponent implements OnInit {
     //    e => console.log(`error: ${e}`),
     //    () => console.log("complete")
     //  );
+
+
+
+  }
+
+  ngAfterViewInit() {
+    // XMLHttpRequest
+    //let button = this.renderer.listen("buttonel", "click", (event) => true);
+    // let button = this.el.nativeElement(this.buttonn);
+    //let button = this.el.nativeElement(this.buttonel);
+    // this.click = Observable.fromEvent(document.getElementById("button"), "click");
+    this.click = Observable.fromEvent(this.buttonn.nativeElement, "click");
+
+    this.click.subscribe(
+      e => this.load("movies.json"),
+      e => console.log(`error: ${e}`),
+      () => console.log("complete")
+    );
   }
 
   myFunction() {
     let myVariable: number = 1;
   }
 
-  onNext(value) {
-    this.circle.nativeElement.style.left = value.x + "px";
-    this.circle.nativeElement.style.top = value.y + "px";
-    // this.circle.nativeElement.style.left = parseInt(value.x);
-    // this.circle.nativeElement.style.top = parseInt(value.y);
+  load(url: string) {
+    let xhr = new XMLHttpRequest();    
+      xhr.addEventListener("load", () => {
+        let moviecollection: Movie[] = this.movies;        
+        //moviecollection.forEach(m => {
+        for(let i = 0; i < moviecollection.length; i++) {
+          let div = document.createElement("div");
+          div.innerText = moviecollection[i].name;
+          //this.output.appendChild(div);
+          //document.getElementById("output").appendChild(div);
+          this.output.nativeElement.appendChild(div);
+
+        };
+    });
+
+    xhr.open("GET", url);
+    xhr.send();
   }
+
+  // onNext(value) {
+  //   this.circle.nativeElement.style.left = value.x + "px";
+  //   this.circle.nativeElement.style.top = value.y + "px";
+  //   // this.circle.nativeElement.style.left = parseInt(value.x);
+  //   // this.circle.nativeElement.style.top = parseInt(value.y);
+  // }
+
+  // load(url: string) {
+  //   let xhr = new XMLHttpRequest();
+
+  //   xhr.addEventListener("load", () => {
+  //     let movies = JSON.parse(xhr.responseText);
+  //     movies.array.forEach(m => {
+  //       let div = document.createElement("div");
+  //       div.innerText = m.name;
+  //       //this.output.appendChild(div);
+  //       //document.getElementById("output").appendChild(div);
+  //       this.output.nativeElement.appendChild(div);
+        
+  //     });
+  //   });
+
+  //   xhr.open("GET", url);
+  //   xhr.send();
+  // }
 
 }
--- CANE.Ngrx.RxJs/src/client/app/movie/movie.service.ts
+++ CANE.Ngrx.RxJs/src/client/app/movie/movie.service.ts
@@ -6,6 +6,7 @@ import { Response } from "@angular/http";
 import { Movie } from "./movie";
 
 const api: string = "/api";
+const localPath: string = "";
 
 @Injectable()
 export class MovieService {
@@ -19,12 +20,26 @@ export class MovieService {
     });
   }
 
-  addMovie(movie: Movie):Observable<Movie> {
-    return this.http.post(`${api}/movie/`, movie)
-    .map((res: Response) => {
-      return res.json() as Movie;
-    });
+  // getMovies() {
+  //   return this.http.get<Movie[]>(`${api}/movies`);    
+  // }
+
+  
+  
+
+  // public getLocalMovies(): Observable<Movie> {
+  //   return this.http.get<Movie>('../assets/movies.json')
+  //   .map((response) =>  response)     
+  // }
+
+  public getLocalMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>('../assets/movies.json');         
   }
+
+  // addMovie(movie: Movie):Observable<Movie[]> {
+  //   return this.http.post(`${api}/movie/`, movie)
+  //   .map((response: Response) => <Movies[]>response.json());
+  // }
 }
 
 
